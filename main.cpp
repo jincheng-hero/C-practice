f2c0d2f0229bf860dac69fbb10c040ca3f4c7d00
jincheng <jincheng201711@163.com>
2024-12-25 10:52:04 +0800

    第一次提交

---
+++ main.cpp
@@ -0,0 +1,19 @@
+//
+// Created by 金铖 on 2024/12/25.
+//
+
+#include "main.h"
+#include <stdio.h>
+
+void test(){
+    printf("hello world5\n");
+}
+
+int main(){
+    printf("hello world1\n");
+    printf("hello world2\n");
+
+    printf("hello world3\n");
+    test();
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by 金铖 on 2024/12/25.
+//
+
+#ifndef C_PRACTICE_MAIN_H
+#define C_PRACTICE_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //C_PRACTICE_MAIN_H
